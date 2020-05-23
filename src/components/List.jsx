f8d785be31554b6915a1fc0bfa81d8bf015f5325
Jesse Bell <jkurtasbell@gmail.com>
2020-05-23 01:22:57 -0700

    Add useState And useEffect To List Component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,10 +1,18 @@
-const React = require('react');
+import React, { useState, useEffect } from 'react';
 
 const List = () => {
+  const [entries, setEntries] = useState([]);
+
+  useEffect(() => {
+    console.log('fetching...')
+    fetch('/query-entries')
+      .then(response => response.json())
+      .then(data => console.log(data));
+  })
 
   return (
     <div>
-      List
+      {entries}
     </div>
   )
 
