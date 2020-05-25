418b795bd0537d11e874ea88cef3f27a2051657e
Jesse Bell <jkurtasbell@gmail.com>
2020-05-24 17:12:47 -0700

    Add handleBookChange Event Function To Filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 
 const Filters = () => {
+  const [whichBook, setWhichBook] = useState('canon');
+
+  const handleBookChange = (e) => {
+    console.log('e', e.target.value)
+    if (whichBook === 'canon') {
+      setWhichBook('research')
+    } else {
+      setWhichBook('canon');
+    }
+    console.log('whichBook', whichBook);
+    fetch('/query-entries', {
+      data: whichBook
+    })
+    .
+
+  }
 
   return (
     <div>
       Filters
+      <p>Book</p>
+        <div>
+        <input type="radio" name="book-filter" id="book-filter" value="canon" onClick={handleBookChange}  />
+          <label for="canon">canon</label>
+        <input type="radio" name="book-filter" id="book-filter" value="research" onClick={handleBookChange} />
+          <label for="research">research</label>
+        </div>
+
     </div>
   )
 }
