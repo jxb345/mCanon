1aecb3613c575bfb962dc9a1b63b1cff03af8680
Jesse Bell <jkurtasbell@gmail.com>
2020-06-29 16:38:44 -0700

    Instal LoremIpsum Library

--- dummydata.js
+++ dummydata.js
@@ -1,5 +1,40 @@
 const { Entry } = require('./connectDb.js');
 const mongoose = require('mongoose');
 const { model } = require('./models.js');
+const LoremIpsum  = require('lorem-ipsum').LoremIpsum;
+
+const lorem = new LoremIpsum({
+  wordsPerSentence: {
+    max: 3,
+    min: 1
+  }
+})
+
+const genres = [
+
+];
+
+const mood = [
+
+];
+
+
+
+let entries = []
+for (let i = 0; i < 200; i += 1) {
+  let singleEntry = {};
+
+  singleEntry.band = lorem.generateSentences(1);
+  singleEntry.album =  lorem.generateSentences(1)
+  singleEntry.genre =  'country',
+  singleEntry.mood =  'chill',
+  singleEntry.instrumental =  'no',
+  singleEntry.year =  1939,
+  singleEntry.rating =  1,
+  singleEntry.book =  'canon',
+}
+
+
+model.insertMany
 
 
