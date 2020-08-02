7c00e35f0577afdda1f32085796ddf89ae0e6317
Jesse Bell <jkurtasbell@gmail.com>
2020-08-01 23:06:34 -0700

    Add Function To Handle Placeholder Text For Search When Focus Or Blur

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,10 +2,19 @@ import React, { useState, useEffect } from 'react';
 
 const Search = (props) => {
   const [query, setQuery] = useState('');
+  const [placeholder, setPlaceholder] = useState('');
 
   const handleChange = (e) => {
     setQuery(e.target.value);
     console.log('query---', query)
+  }
+
+  const addPlaceholder = (e) => {
+    setPlaceholder('search for band or album...');
+  }
+
+  const removePlaceholder = (e) => {
+    setPlaceholder('');
   }
 
   useEffect( () => {
@@ -44,7 +53,7 @@ const Search = (props) => {
       {/* moved to App.Jsx
      <button className="new-entry-btn" onClick={displayForm}>+</button> */}
       <div className="input-search">
-      <input type="text" onChange={handleChange} />
+      <input type="text" placeholder={placeholder} onChange={handleChange} onClick={addPlaceholder} onBlur={removePlaceholder} />
       </div>
     </div>
   )
