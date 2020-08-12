0bc2038e97fd77f60ee2d93232cce4a12711758d
Jesse Bell <jkurtasbell@gmail.com>
2020-08-12 14:43:51 -0700

    Add Login.jsx Component

--- passport.js
+++ passport.js
@@ -1,9 +1,21 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
 const mongoose = require('mongoose');
+const { model } = require('./models.js');
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    username.f
+    model.findOne({username: username}), function (err, user) {
+      if (err) return done(err);
+
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.'});
+      }
+
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.'});
+      }
+      return done( null, user);
+    }
   }
 ))
---
+++ src/components/Login.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+
+const Login = () => {
+
+  return (
+    <div>
+      <form action="/login" method="post">
+        <div>
+          <label>Username:</label>
+          <input type="text" name="username"/>
+        </div>
+        <div>
+        <label>Password:</label>
+          <input type="text" name="password"/>
+        </div>
+        <div>
+          <input type="submit" value="Log In"/>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Login
