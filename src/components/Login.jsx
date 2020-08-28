a1d9ac735befacfd26f822307ab42827d6e71f78
Jesse Bell <jkurtasbell@gmail.com>
2020-08-28 13:22:31 -0700

    Add handleTabs Function To Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,45 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Login = (props) => {
 
-  // const [ username, setUsername ] = useState('');
-  // const [ password, setPassword ] = useState('');
+  // copied directly from Form.jsx - probably move this to
+  // App.jsx and have Login.jsx and Form.jsx
+  // use this function as a prop
+  const handleTabs = (action) => {
+    // added these 4 const(s) inside handleTabs
+    // b/c when they were in the functional scope
+    // they 4 consts were undefined
+    const signinCredentials = document.getElementsByClassName("signup-credentials")[0];
+    const loginCredentials = document.getElementsByClassName("login-credentials")[0];
+    const loginButtonClick = document.getElementById("login");
+    const signinButtonClick = document.getElementById("signup");
 
-  // let redirectToHome = false;
+    console.log('login', loginCredentials)
+    // const loginElement = loginCredentials[0]
+    // const signinElement = signinCredentials[0]
+    console.log('signin', signinCredentials)
 
-  // const handleChange = (e) => {
-  //   e.preventDefault();
-  //   console.log('e.id', e.target.id)
-  //   if (e.target.id === 'login-username' || e.target.id === 'signup-username') {
-  //     setUsername(e.target.value, ...username)
-  //     console.log('username', username);
-  //   } else {
-  //     setPassword(e.target.value, ...password)
-  //     console.log('password', password);
-
-  //   }
-  // }
-
-  // const login = () => {
-  //   const credentials = {
-  //     username: username,
-  //     password: password
-  //   }
-  //   console.log('creditials------', credentials)
-  //   fetch('/login', {
-  //     method: 'post',
-  //     headers: {
-  //       'Content-type': 'application/json'
-  //     },
-  //     body: JSON.stringify(credentials),
-  //   })
-  //   .then(data => {
-  //     console.log('data',)
-  //     redirectToHome = true;
-  //     console.log('redirectToHome', redirectToHome)
-  //   })
-  // }
-
+    if (action === 'login') {
+      loginButtonClick.style.opacity = '100%'
+      loginCredentials.style.display = 'block';
+      signinButtonClick.style.opacity = '40%'
+      signinCredentials.style.display = 'none';
+    } else {
+      loginCredentials.style.display = 'none';
+      loginButtonClick.style.opacity = '40%';
+      signinCredentials.style.display = 'block';
+      signinButtonClick.style.opacity = '100%'
+    }
+  }
 
   return (
         <div>
@@ -47,34 +38,28 @@ const Login = (props) => {
             mCanon
           </div>
           <br/>
-          <div className="login-popup">
-
           <div className="tabs-new-entry">
           <div className="form">
           <div className="tabs-new-entry-links">
-            <button type="button" id="login">
+            <button type="button" id="login" onClick={ () => { handleTabs('login') }}>
               LOGIN
             </button>
-            <button type="button" id="sign-up" >
+            <button type="button" id="signup" onClick={ () => { handleTabs('signup') }}>
               SIGN UP
             </button>
           </div>
-          <div>
-            <div>
+          <div className="login-credentials">
               <label>Username:</label>
               <input type="text" name="username" id="login-username" onChange={props.handleChange}/>
-            </div>
             <div>
             <label>Password:</label>
-              <input type="text" name="password" id="lgoin-password" onChange={props.handleChange} />
+              <input type="text" name="password" id="login-password" onChange={props.handleChange} />
             </div>
             <div>
               <button onClick={props.verifyAuth} type="submit" value="login"/>
             </div>
           </div>
-
-          {/* <form action="/signup" method="post"> */}
-            <div>
+            <div className="signup-credentials">
               <label>Username:</label>
               <input type="text" name="username" id="signup-username" onChange={props.handleChange}/>
             </div>
@@ -85,12 +70,8 @@ const Login = (props) => {
             <div>
               <button onClick={props.verifyAuth} value="signup"/>
             </div>
-          {/* </form> */}
         </div>
         </div>
-        <div>
-        </div>
-          </div>
     </div>
 
   )
